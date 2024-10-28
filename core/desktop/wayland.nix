25b5fb4f346b538d56b7a597480c0b02d71eb51c
alisa <alisa@alisaqaq.moe>
2024-10-28 16:47:12 +0800

    feat: add sddm theme

--- core/desktop/wayland.nix
+++ core/desktop/wayland.nix
@@ -16,4 +16,30 @@
       pkgs.xdg-desktop-portal-gtk
     ];
   };
+
+  services.displayManager.sddm = {
+    enable = true;
+    theme = "catppuccin-mocha";
+    package = pkgs.kdePackages.sddm;
+    autoNumlock = true;
+    wayland.enable = true;
+    extraPackages = with pkgs.kdePackages; [
+      breeze-icons
+      qtsvg
+      qtvirtualkeyboard
+      kirigami
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    (catppuccin-sddm.override {
+      flavor = "mocha";
+      font = "Noto Sans";
+      fontSize = "9";
+      background = "${../../images/login-background.png}";
+      loginBackground = true;
+    })
+    inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
+    xdg-desktop-portal-gtk
+  ];
 }
