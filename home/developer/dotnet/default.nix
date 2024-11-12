5c140f6c4fdd1fa572b6ece42adfa4edd4e4762d
alisa <alisa@alisaqaq.moe>
2024-11-12 19:10:57 +0800

    fix: able to install and update dotnet workload

--- home/developer/dotnet/default.nix
+++ home/developer/dotnet/default.nix
@@ -1,32 +1,34 @@
 { pkgs, ... }:
 
 let
-  dotnet-combined =
-    (
-      with pkgs.dotnetCorePackages;
+  # This is needed to install workload in $HOME
+  # https://discourse.nixos.org/t/dotnet-maui-workload/20370/2
+  userlocal =  ''
+      for i in $out/sdk/*; do
+        i=$(basename $i)
+        length=$(printf "%s" "$i" | wc -c)
+        substring=$(printf "%s" "$i" | cut -c 1-$(expr $length - 2))
+        i="$substring""00"
+        mkdir -p $out/metadata/workloads/''${i/-*}
+        touch $out/metadata/workloads/''${i/-*}/userlocal
+      done
+    '';
+
+  # append userlocal sctipt to postInstall phase
+  postInstallUserlocal = (finalAttrs: previousAttrs: {
+    postInstall = (previousAttrs.postInstall or '''') + userlocal;
+  });
+  # append userlocal sctipt to postBuild phase
+  postBuildUserlocal = (finalAttrs: previousAttrs: {
+    postBuild = (previousAttrs.postBuild or '''') + userlocal;
+  });
+
+  dotnet-combined = (with pkgs.dotnetCorePackages;
       combinePackages [
-        sdk_8_0
+        (sdk_8_0.overrideAttrs postInstallUserlocal)
+        (sdk_9_0.overrideAttrs postInstallUserlocal)
       ]
-    ).overrideAttrs
-      (
-        finalAttrs: previousAttrs: {
-          # This is needed to install workload in $HOME
-          # https://discourse.nixos.org/t/dotnet-maui-workload/20370/2
-          postBuild =
-            (previousAttrs.postBuild or '''')
-            + ''
-               for i in $out/sdk/*
-               do
-                 i=$(basename $i)
-                 length=$(printf "%s" "$i" | wc -c)
-                 substring=$(printf "%s" "$i" | cut -c 1-$(expr $length - 2))
-                 i="$substring""00"
-                 mkdir -p $out/metadata/workloads/''${i/-*}
-                 touch $out/metadata/workloads/''${i/-*}/userlocal
-              done
-            '';
-        }
-      );
+    ).overrideAttrs postBuildUserlocal;
 in
 {
   home.sessionVariables = {
@@ -37,3 +39,4 @@ in
     dotnet-combined
   ];
 }
+
