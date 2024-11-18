c16254346bb200eda355019b0a5e484c5afdc0db
alisa <alisa@alisaqaq.moe>
2024-11-18 15:21:52 +0800

    fix: use keychain for git credential store on macOS

--- home/terminal/programs/git.nix
+++ home/terminal/programs/git.nix
@@ -1,5 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
+let 
+  gitCredentialStore = if pkgs.stdenvNoCC.isDarwin then "keychain" else "cache";
+inda
 {
   home.packages = with pkgs; [
     gitFull
@@ -25,7 +28,7 @@
           gitlabDevClientId = "e69becbbc576cc44c376078f5a5c0e3d8bc3bcc108ef1c63e8f8904facfed786";
           gitlabDevClientSecret = "gloas-1bf10b41754d4f719bc9088a39f68a2676c0c8e72ded3f3275f2bfb0d8dad446";
         };
-        credentialStore = "cache";
+        credentialStore = gitCredentialStore;
       };
     };
     signing = {
