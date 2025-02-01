08db07280b431dc72c0a33194fb8c1a5555ed7c2
alisa <alisa@alisaqaq.moe>
2025-02-01 15:24:45 +0800

    feat: use proxy for nix daemon on wsl

--- core/galaxy-wsl2-core.nix
+++ core/galaxy-wsl2-core.nix
@@ -9,4 +9,9 @@
   environment.sessionVariables = {
     FLAKE = "/home/alisa/.nixos-config";
   };
+
+  systemd.services.nix-daemon.environment = {
+    http_proxy = "http://127.0.0.1:7890";
+    https_proxy = "http://127.0.0.1:7890";
+  };
 }
