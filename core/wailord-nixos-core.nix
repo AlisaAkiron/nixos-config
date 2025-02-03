99d8490f8e0e6e99d8875d3bd43744340c9d43cb
alisa <alisa@alisaqaq.moe>
2025-02-03 19:11:34 +0800

    feat: add ssh key for pgadmin on wailord

--- core/wailord-nixos-core.nix
+++ core/wailord-nixos-core.nix
@@ -13,4 +13,9 @@
 
   # use firewall provided by cloud provider
   networking.firewall.enable = false;
+
+  # pgadmin's ssh key
+  users.users.alisa.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICNXiJoAalVVCuK/1psQiKN4NNIHndYApBHqkYIHhVqq pgadmin"
+  ];
 }
