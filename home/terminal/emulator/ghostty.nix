e5f4ad87e3c7dc4ae86d29ac1034608d07f2507c
alisa <alisa@alisaqaq.moe>
2025-03-09 21:19:44 +0800

    feat: use ghostty native tab feature

--- home/terminal/emulator/ghostty.nix
+++ home/terminal/emulator/ghostty.nix
@@ -13,20 +13,10 @@ in
     font-family = "Maple Mono NF"
     font-size = 16
     theme = dark:catppuccin-mocha,light:catppuccin-latte
+    window-padding-balance = true
     window-padding-color = background
     cursor-style = bar
     cursor-style-blink = true
     shell-integration-features = no-cursor
-
-    # for zellij
-    macos-option-as-alt = true
-    keybind = cmd+t=unbind
-    keybind = cmd+n=unbind
-    keybind = cmd+c=unbind
-    keybind = cmd+w=unbind
-    keybind = alt+left=unbind
-    keybind = alt+right=unbind
-    keybind = cmd+opt+left=unbind
-    keybind = cmd+opt+right=unbind
   '';
 }
