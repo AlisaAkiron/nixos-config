26dcb3000f683dc7efac531ce8d8d9c8b95ddaef
alisa <alisa@alisaqaq.moe>
2025-08-13 02:44:35 +0800

    feat: add docker daemon proxy for neptune server vm

--- core/neptune-vm-nixos.core.nix
+++ core/neptune-vm-nixos.core.nix
@@ -8,4 +8,11 @@
   ];
 
   virtualisation.docker.storageDriver = "btrfs";
+  virtualisation.docker.daemon.settings = {
+    proxies = {
+      "http-proxy" = "http://mihomo.pikachu.alisaqaq.moe:7890";
+      "https-proxy" = "http://mihomo.pikachu.alisaqaq.moe:7890";
+      "no-proxy" = "localhost,127.0.0.0/8,10.0.0.0/8,*.alisaqaq.moe";
+    };
+  };
 }
