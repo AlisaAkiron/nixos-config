39982bb7c079a01d7d95a820de2804de78a28d4e
alisa <alisa@alisaqaq.moe>
2025-08-13 23:30:10 +0800

    feat: add swap on blissey

--- hosts/neptune/blissey/default.nix
+++ hosts/neptune/blissey/default.nix
@@ -7,4 +7,11 @@
 
   networking.hostName = "blissey";
   system.stateVersion = "25.05";
+
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 8 * 1024; # 8 GB
+    }
+  ];
 }
