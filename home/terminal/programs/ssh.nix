a890c1c7c7af54fa70fae90e64d8859aac94a95c
alisa <alisa@alisaqaq.moe>
2025-08-29 00:09:42 +0800

    feat: configure SSH settings defaults

--- home/terminal/programs/ssh.nix
+++ home/terminal/programs/ssh.nix
@@ -3,5 +3,26 @@
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
+    matchBlocks = {
+      "*" = {
+        # security
+        forwardAgent = false;
+        forwardX11 = false;
+        # keep alive
+        serverAliveInterval = 60;
+        serverAliveCountMax = 3;
+        # multiplexing
+        controlMaster = "auto";
+        controlPath = "~/.ssh/sockets/%r@%h-%p";
+        controlPersist = "10m";
+        # known hosts
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        # miscellaneous
+        addKeysToAgent = "no";
+        compression = true;
+      };
+    };
   };
 }
