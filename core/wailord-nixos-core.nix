ee2b5041dfb6bf4327501cf81a36bf7a0689fce6
alisa <alisa@alisaqaq.moe>
2025-12-25 01:58:48 +0800

    feat: add rustdesk signal relay hosts configuration

--- core/wailord-nixos-core.nix
+++ core/wailord-nixos-core.nix
@@ -15,6 +15,11 @@
   # use firewall provided by cloud provider
   networking.firewall.enable = false;
 
+  # rust desk signal relay hosts
+  services.rustdesk-server.signal.relayHosts = [
+    "wailord.alisaqaq.moe"
+  ];
+
   # pgadmin's ssh key
   users.users.alisa.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICNXiJoAalVVCuK/1psQiKN4NNIHndYApBHqkYIHhVqq pgadmin"
