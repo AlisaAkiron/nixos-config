f69d4a3368ad5c3fe095e21988e46e85781f8075
alisa <alisa@alisaqaq.moe>
2026-01-22 02:46:08 +0800

    feat: add Prometheus node exporter configuration

--- core/profiles/homelab-server.nix
+++ core/profiles/homelab-server.nix
@@ -31,4 +31,16 @@
       ];
     };
   };
+
+  services.prometheus.exporters.node = {
+    enable = true;
+    listenAddress = "0.0.0.0";
+    port = 9100;
+    enabledCollectors = [
+      "ethtool"
+      "softirqs"
+      "systemd"
+      "tcpstat"
+    ];
+  };
 }
